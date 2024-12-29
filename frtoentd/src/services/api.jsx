5ce3381860de35c9a5a6cc578798508b2be52cb4
sourav R <128279891+souravsouru7@users.noreply.github.com>
2024-12-29 09:15:19 +0530

    Update api.jsx

--- frtoentd/src/services/api.jsx
+++ frtoentd/src/services/api.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-    baseURL: 'http://localhost:3000/', // Update with your backend URL
+    baseURL: 'www.modernbakery.shop/', // Update with your backend URL
 });
 
 export const signup = (data) => API.post('/auth/signup', data);
