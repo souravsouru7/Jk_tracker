98c817255f630cfe07ef60626faf57ba777272b1
sourav R <128279891+souravsouru7@users.noreply.github.com>
2024-12-29 09:16:04 +0530

    Update entrySlice.jsx

--- frtoentd/src/store/slice/entrySlice.jsx
+++ frtoentd/src/store/slice/entrySlice.jsx
@@ -2,7 +2,7 @@ import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:3000/',
+  baseURL: 'www.modernbakery.shop/',
 });
 
 const validateEntry = (entry) => {
